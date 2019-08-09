87aab6fbd53a56de222a7f3a25fdaeb855fc19ee
unknown <dj.zaragoza@icloud.com>
2019-08-08 18:01:23 -0700

    still working on accounts-router.js file

--- accounts/accounts-router.js
+++ accounts/accounts-router.js
@@ -29,3 +29,34 @@ router.get('/:id', async (req, res) => {
             res.status(500).json({ success: false, error: 'There was an error while finding the account.', err });
         }
 });
+
+router.post('/', async (req, res) => {
+    const accountData = req.body;
+
+    try {
+        const account = await
+        db('accounts').insert(accountData);
+        res.status(201).json({ success: true, account });
+    } catch (err) {
+        res.status(500).json({ success: false, error: 'There was an error while adding your account data.', err });
+    }
+});
+
+router.put('/:id', async (req, res) => {
+    const {id} = req.params;
+    const update = req.body;
+
+    try {
+        const account = await
+        db('accounts').where({id}).update(update);
+        if(account) {
+            res.status(200).json({ success: true, account});
+        } else {
+            res.status(404).json({ success: false, error: `Could not find account with ${id}` });
+        }
+    } catch (err) {
+        res.status(500).json({ success: true, error: `There was an error while updating the account.`, err});
+    }
+
+});
+
